5906714e7976260f8a93ef8b1443530b93c3ae89
Chawku <chawku.work@gmail.com>
2024-12-06 19:10:12 +0300

    update

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -1,6 +1,6 @@
 import os
-
 import psycopg2
+from psycopg2.extras import DictCursor
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,34 +9,33 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 def get_url_id(url_string):
     with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as curs:
+        with conn.cursor(cursor_factory=DictCursor) as curs:
             get_ids_of_url_query = "SELECT id from urls WHERE name= %s;"
             curs.execute(get_ids_of_url_query, (url_string,))
-            urls_tuples = curs.fetchall()
-            if urls_tuples:
-                return urls_tuples[0][0]
+            urls_dicts = curs.fetchall()
+            if urls_dicts:
+                return urls_dicts[0]['id']
             else:
                 return None
 
 
 def add_url(url_string):
     with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as curs:
+        with conn.cursor(cursor_factory=DictCursor) as curs:
             add_url_query = (
                 "INSERT INTO urls (name, created_at) "
                 "VALUES (%s, NOW()) "
                 "RETURNING id;"
             )
-
             curs.execute(add_url_query, (url_string,))
-            url_id = curs.fetchone()[0]
+            url_id = curs.fetchone()['id']
             conn.commit()
             return url_id
 
 
 def get_all_urls():
     with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
             sql_query = """
             SELECT
                 u.id AS url_id,
@@ -58,22 +57,22 @@ def get_all_urls():
             ORDER BY u.created_at DESC;
             """
             cur.execute(sql_query)
-            urls_tuples = cur.fetchall()
-            return urls_tuples
+            urls_dicts = cur.fetchall()
+            return urls_dicts
 
 
 def get_url_data(id):
     with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
             get_url_data_query = "SELECT * FROM urls where id=%s ;"
             cur.execute(get_url_data_query, (id,))
-            urls_tuples = cur.fetchall()
-            return urls_tuples
+            urls_dicts = cur.fetchall()
+            return urls_dicts
 
 
 def get_url_checks_data(id):
     with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
             get_url_checks_data = (
                 "SELECT id, status_code, h1, title, content, created_at "
                 "FROM url_checks "
@@ -81,13 +80,13 @@ def get_url_checks_data(id):
                 "ORDER BY created_at DESC;"
             )
             cur.execute(get_url_checks_data, (id,))
-            url_checks_tuples = cur.fetchall()
-            return url_checks_tuples
+            url_checks_dicts = cur.fetchall()
+            return url_checks_dicts
 
 
 def add_url_check(params):
     with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
             request_string = (
                 "INSERT INTO url_checks ("
                 "url_id, status_code, created_at, h1, title, content"
