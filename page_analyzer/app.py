cc2b36ddd361a9dd444d58ff0e13525c66004495
Chawku <chawku.work@gmail.com>
2024-12-06 18:55:58 +0300

    update

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,15 @@
 from urllib.parse import urlparse
 
-from flask import Flask, redirect, render_template, request, url_for
+import validators
+from flask import (
+    Flask,
+    flash,
+    get_flashed_messages,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 
 from .database import (
     add_url,
@@ -11,46 +20,53 @@ from .database import (
     get_url_id,
 )
 
-from .utils import make_request, parse_html, get_messages, flash_message, is_valid_url, normalize_url
+from .utils import check_url
+
 
 app = Flask(__name__)
 app.secret_key = 'secret key'
 
+
 @app.route('/')
 def index():
-    messages = get_messages()
+    messages = get_flashed_messages(with_categories=True)
     return render_template("index.html", messages=messages)
+
 
 @app.post('/urls')
 def urls_page():
     url_string = request.form.to_dict().get('url', '')
-    if not is_valid_url(url_string):
+    if not validators.url(url_string):
         messages = [("alert alert-danger", "Некорректный URL")]
         return render_template("index.html", messages=messages), 422
-    url_string = normalize_url(url_string)
+    url_string = urlparse(url_string)
+    url_string = f'{url_string.scheme}://{url_string.netloc}'
     if url_string:
         url_id = get_url_id(url_string)
         if url_id:
-            flash_message("Страница уже существует", "alert alert-danger")
+            flash("Страница уже существует", "alert alert-danger")
         else:
             url_id = add_url(url_string)
-            flash_message("Страница успешно добавлена", "alert alert-success")
+            flash("Страница успешно добавлена", "alert alert-success")
         return redirect(url_for('get_url', id=url_id)), 301
+
 
 @app.get('/urls')
 def get_urls():
-    messages = get_messages()
+    messages = get_flashed_messages(with_categories=True)
     urls_tuples = get_all_urls()
     urls_list = []
     for url_tuple in urls_tuples:
         id, name, status, date = url_tuple
         date = (date.date() if date else '')
-        urls_list.append({'id': id, 'name': name, 'check_date': date, 'status': status})
+        urls_list.append({'id': id, 'name': name,
+                          'check_date': date, 'status': status})
     return render_template("urls.html", urls=urls_list, messages=messages)
+
 
 @app.get('/urls/<id>')
 def get_url(id):
-    messages = get_messages()
+    messages = get_flashed_messages(with_categories=True)
     urls_tuples = get_url_data(id)
     id, name, date = urls_tuples[0]
     urls_data = {"id": id, "name": name, "date": date.date()}
@@ -59,23 +75,28 @@ def get_url(id):
     if url_checks_tuples:
         for url_check_tuple in url_checks_tuples:
             id, status, h1, title, content, date = url_check_tuple
-            url_checks_list.append({'id': id, 'status': status, 'h1': h1, 'title': title, 'content': content, 'date': date.date()})
-    return render_template("url.html", url=urls_data, url_checks=url_checks_list, messages=messages)
+            url_checks_list.append({'id': id, 'status': status, 'h1': h1,
+                                    'title': title, 'content': content,
+                                    'date': date.date()})
+    return render_template("url.html", url=urls_data,
+                           url_checks=url_checks_list, messages=messages)
+
 
 @app.post('/urls/<id>/checks')
-def check_url(id):
+def check_url_route(id):
     urls_tuples = get_url_data(id)
     if urls_tuples:
         name = urls_tuples[0][1]
-    req = make_request(name)
-    if not req:
-        flash_message("Произошла ошибка при проверке", "alert alert-danger")
+
+    h1, title, content, status_code = check_url(name)
+    if status_code != 200:
+        flash("Произошла ошибка при проверке", "alert alert-danger")
         return redirect(url_for('get_url', id=id))
-    html_content = req.text
-    parsed_data = parse_html(html_content)
-    params = {'check_id': id, 'status_code': req.status_code, 'title': parsed_data['title'], 'h1': parsed_data['h1'], 'content': parsed_data['content']}
+
+    params = {'check_id': id, 'status_code': status_code,
+              'title': title, 'h1': h1, 'content': content}
     add_url_check(params)
-    flash_message("Страница успешно проверена", "alert alert-success")
+    flash("Страница успешно проверена", "alert alert-success")
     return get_url(id)
 
 if __name__ == "__main__":
--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -1,48 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
-from flask import flash, get_flashed_messages
-import validators
-from urllib.parse import urlparse
 
 
-def is_valid_url(url_string):
-    return validators.url(url_string)
-
-
-def normalize_url(url_string):
-    url_string = urlparse(url_string)
-    return f'{url_string.scheme}://{url_string.netloc}'
-
-
-def make_request(url):
+def check_url(url):
     try:
-        req = requests.request("GET", url)
+        req = requests.get(url)
         status_code = req.status_code
         if status_code != 200:
             raise requests.RequestException
-        return req
     except requests.RequestException:
-        return None
+        return None, None, None, None, status_code
 
-
-def parse_html(html_content):
+    html_content = req.text
     soup = BeautifulSoup(html_content, 'html.parser')
+
     h1 = soup.find('h1')
     h1 = h1.text if h1 else ''
+
     title = soup.find('title')
     title = title.text if title else ''
+
     attrs = {'name': 'description'}
     meta_description_tag = soup.find('meta', attrs=attrs)
     content = ''
     if meta_description_tag:
         content = meta_description_tag.get("content")
         content = content if content else ''
-    return {'h1': h1, 'title': title, 'content': content}
 
-
-def get_messages():
-    return get_flashed_messages(with_categories=True)
-
-
-def flash_message(message, category):
-    flash(message, category)
+    return h1, title, content, status_code
