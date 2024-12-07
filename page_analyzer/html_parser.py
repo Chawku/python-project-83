3e8da9b44f78d6883a7d32926c9e453fe8218646
Chawku <chawku.work@gmail.com>
2024-12-07 19:04:26 +0300

    update

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -6,7 +6,8 @@ def extract_page_data(html_content):
     h1 = soup.find('h1').text if soup.find('h1') else ''
     title = soup.find('title').text if soup.find('title') else ''
     meta_description_tag = soup.find('meta', attrs={'name': 'description'})
-    content = meta_description_tag.get("content") if meta_description_tag else ''
+    content = (meta_description_tag.get("content")
+           if meta_description_tag else '')
     
     return {
         'h1': h1,
