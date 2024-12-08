8c262b72e40ea80dec560833a0ab305762b3f8e7
Chawku <chawku.work@gmail.com>
2024-12-08 21:43:12 +0300

    update

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -72,8 +72,8 @@ def find_url_by_id(id):
                 WHERE id = %s;
             """
             cur.execute(find_url_by_id_query, (id,))
-            url_dict = cur.fetchone()
-            return url_dict
+            urls_dicts = cur.fetchone()
+            return urls_dicts
 
 
 def get_url_checks_data(id):
