bcd187f312073711af6b072ca791074e778e84c8
Chawku <chawku.work@gmail.com>
2024-12-08 21:27:42 +0300

    update

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -11,7 +11,11 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 def find_by_url(url_string):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=DictCursor) as curs:
-            get_ids_of_url_query = "SELECT id FROM urls WHERE name = %s;"
+            get_ids_of_url_query = """
+                SELECT id 
+                FROM urls 
+                WHERE name = %s;
+            """
             curs.execute(get_ids_of_url_query, (url_string,))
             url_record = curs.fetchone()
             return url_record['id'] if url_record else None
@@ -21,9 +25,10 @@ def add_url(url_string):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=DictCursor) as curs:
             add_url_query = """
-            INSERT INTO urls (name, created_at)
-            VALUES (%s, NOW()) "
-            RETURNING id;"""
+                INSERT INTO urls (name, created_at) 
+                VALUES (%s, NOW()) 
+                RETURNING id;
+            """
             curs.execute(add_url_query, (url_string,))
             url_id = curs.fetchone()['id']
             conn.commit()
@@ -34,24 +39,24 @@ def get_all_urls():
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
             sql_query = """
-            SELECT
-                u.id AS url_id,
-                u.name AS url_name,
-                COALESCE(uc.status_code, '') AS status_code,
-                uc.created_at AS max_created_at
-            FROM
-                urls u
-            LEFT JOIN (
                 SELECT
-                    url_id,
-                    status_code,
-                    created_at,
-                    ROW_NUMBER() OVER (PARTITION BY
-                    url_id ORDER BY created_at DESC) AS row_num
+                    u.id AS url_id,
+                    u.name AS url_name,
+                    COALESCE(uc.status_code, '') AS status_code,
+                    uc.created_at AS max_created_at
                 FROM
-                    url_checks
-            ) uc ON u.id = uc.url_id AND uc.row_num = 1
-            ORDER BY u.created_at DESC;
+                    urls u
+                LEFT JOIN (
+                    SELECT
+                        url_id,
+                        status_code,
+                        created_at,
+                        ROW_NUMBER() OVER (PARTITION BY
+                        url_id ORDER BY created_at DESC) AS row_num
+                    FROM
+                        url_checks
+                ) uc ON u.id = uc.url_id AND uc.row_num = 1
+                ORDER BY u.created_at DESC;
             """
             cur.execute(sql_query)
             urls_dicts = cur.fetchall()
@@ -61,7 +66,11 @@ def get_all_urls():
 def get_url_data(id):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            get_url_data_query = "SELECT * FROM urls where id=%s ;"
+            get_url_data_query = """
+                SELECT * 
+                FROM urls 
+                WHERE id = %s;
+            """
             cur.execute(get_url_data_query, (id,))
             urls_dicts = cur.fetchall()
             return urls_dicts
@@ -71,10 +80,15 @@ def get_url_checks_data(id):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
             get_url_checks_data = """
-            SELECT id, status_code, h1, title, content, created_at
-            FROM url_checks
-            WHERE url_id=%s
-            ORDER BY created_at DESC;"""
+                SELECT 
+                    id, status_code, h1, title, content, created_at 
+                FROM 
+                    url_checks 
+                WHERE 
+                    url_id = %s 
+                ORDER BY 
+                    created_at DESC;
+            """
             cur.execute(get_url_checks_data, (id,))
             url_checks_dicts = cur.fetchall()
             return url_checks_dicts
@@ -84,12 +98,14 @@ def add_url_check(params):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
             request_string = """
-            INSERT INTO url_checks (
-            url_id, status_code, created_at, h1, title, content
-            ) VALUES (
-            %s, %s, NOW(), %s, %s, %s
-            );"""
-            cur.execute(request_string, (params['check_id'],
-                                         params['status_code'], params['h1'],
-                                         params['title'], params['content']))
+                INSERT INTO url_checks (
+                    url_id, status_code, created_at, h1, title, content
+                ) VALUES (
+                    %s, %s, NOW(), %s, %s, %s
+                );
+            """
+            cur.execute(request_string, (
+                params['check_id'], params['status_code'], 
+                params['h1'], params['title'], params['content']
+            ))
             conn.commit()
